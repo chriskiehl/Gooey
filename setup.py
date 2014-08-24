7494eb338adb3a381a8c86efb7bcaa994d7879a7
chriskiehl <audionautic@gmail.com>
2014-08-24 18:44:19 -0400

    Made azeirah's suggested changes to setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,27 @@ with open('README.md') as readme:
 
 local_path = os.path.join(os.path.dirname(__file__), 'gooey') 
 
-images = [image for image in os.listdir(os.path.join(local_path, 'images'))]
+imagepath = os.path.join(local_path, 'images')
+langpath  = os.path.join(local_path, 'languages')
 
-languages = [lang 
-            for lang in os.listdir(os.path.join(local_path, 'languages'))
+images = [os.path.join(imagepath, image) for image in os.listdir(imagepath)]
+
+languages = [os.path.join(langpath, lang)
+            for lang in os.listdir(langpath)
             if '.py' not in lang]
+
+print(images)
+print(languages)
 
 setup(
     name='Gooey',
     version='0.1.0',
     author='Chris Kiehl',
     author_email='ckiehl@gmail.com',
+    data_files=[
+      ('', images), 
+      ('', languages)
+    ],
     packages=[
       'gooey',
       'gooey.gui',
@@ -25,10 +35,6 @@ setup(
       'gooey.mockapplications',
     ],
 
-    data_files=[
-      ('gooey/images', images), 
-      ('gooey/languages', languages)
-    ],
     url='http://pypi.python.org/pypi/Gooey/',
     license='LICENSE.txt',
     description='Turn (almost) any command line program into a full GUI application with one line',
