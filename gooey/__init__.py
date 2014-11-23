79ae55b8753fc2718bf5b1caaddbd3075fce013b
chriskiehl <audionautic@gmail.com>
2014-11-22 21:24:48 -0500

    fixed path error in __init__

--- gooey/__init__.py
+++ gooey/__init__.py
@@ -1,8 +1,5 @@
-try:
-    from gooey_decorator import Gooey
-    from gooey.python_bindings.gooey_parser import GooeyParser
-except ImportError:
-    pass
+from gooey.python_bindings.gooey_decorator import Gooey
+from gooey.python_bindings.gooey_parser import GooeyParser
 
 __version__ = '0.1.0'
 
