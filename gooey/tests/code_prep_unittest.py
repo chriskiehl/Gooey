5cf42a17b88a97b3bf6a498e184264bafb68e75b
chriskiehl <audionautic@gmail.com>
2015-05-19 21:46:19 -0400

    Removed old tests. Cleaned up new tests

--- gooey/tests/code_prep_unittest.py
+++ gooey/tests/code_prep_unittest.py
@@ -1,57 +1,55 @@
 __author__ = 'Chris'
 
 import unittest
-import code_prep
+from gooey.python_bindings import code_prep
 
-class TestCodePrep(unittest.TestCase):
 
-  def test_split_line(self):
-    line = "parser = ArgumentParser(description='Example Argparse Program')"
-    self.assertEqual("parser", code_prep.split_line(line)[0])
-    self.assertEqual("ArgumentParser(description='Example Argparse Program')", code_prep.split_line(line)[1])
+def test_split_line():
+  line = "parser = ArgumentParser(description='Example Argparse Program')"
+  assert "parser" == code_prep.split_line(line)[0]
+  assert "ArgumentParser(description='Example Argparse Program')" == code_prep.split_line(line)[1]
 
-  def test_update_parser_varname_assigns_new_name_to_parser_var(self):
-    line = ["parser = ArgumentParser(description='Example Argparse Program')"]
-    self.assertEqual(
-      "jarser = ArgumentParser(description='Example Argparse Program')",
-      code_prep.update_parser_varname('jarser', line)[0]
-    )
 
-  def test_update_parser_varname_assigns_new_name_to_parser_var__multiline(self):
-    lines = '''
+def test_update_parser_varname_assigns_new_name_to_parser_var():
+  line = ["parser = ArgumentParser(description='Example Argparse Program')"]
+  expected = "jarser = ArgumentParser(description='Example Argparse Program')"
+  result = code_prep.update_parser_varname('jarser', line)[0]
+  assert result == expected
+
+def test_update_parser_varname_assigns_new_name_to_parser_var__multiline():
+  lines = '''
 import argparse
 from argparse import ArgumentParser
 parser = ArgumentParser(description='Example Argparse Program')
 parser.parse_args()
-    '''.split('\n')
+  '''.split('\n')
 
-    self.assertEqual(
-      "jarser = ArgumentParser(description='Example Argparse Program')",
-      code_prep.update_parser_varname('jarser', lines)[2]
-    )
+  line = "jarser = ArgumentParser(description='Example Argparse Program')"
+  result = code_prep.update_parser_varname('jarser', lines)[2]
+  assert line == result
 
-  def test_take_imports_drops_all_non_imports_statements(self):
-    lines = '''
+
+def test_take_imports_drops_all_non_imports_statements():
+  lines = '''
 import argparse
 from argparse import ArgumentParser
 parser = ArgumentParser(description='Example Argparse Program')
 parser.parse_args()
-    '''.split('\n')[1:]
+  '''.split('\n')[1:]
 
-    self.assertEqual(2, len(list(code_prep.take_imports(lines))))
-    self.assertEqual('import argparse', list(code_prep.take_imports(lines))[0])
+  assert 2 == len(list(code_prep.take_imports(lines)))
+  assert 'import argparse' == list(code_prep.take_imports(lines))[0]
 
-  def test_drop_imports_excludes_all_imports_statements(self):
-    lines = '''
+
+def test_drop_imports_excludes_all_imports_statements():
+  lines = '''
 import argparse
 from argparse import ArgumentParser
 parser = ArgumentParser(description='Example Argparse Program')
 parser.parse_args()
-    '''.split('\n')[1:]
+  '''.split('\n')[1:]
 
-    self.assertEqual(2, len(list(code_prep.take_imports(lines))))
-    self.assertEqual('parser.parse_args()', list(code_prep.drop_imports(lines))[1])
+  assert 2 == len(list(code_prep.take_imports(lines)))
+  assert 'parser.parse_args()' == list(code_prep.drop_imports(lines))[1]
 
-if __name__ == "__main__":
-  #import sys;sys.argv = ['', 'Test.testName']
-  unittest.main()
+
--- gooey/tests/modules_unittest.py
+++ gooey/tests/modules_unittest.py
@@ -1,23 +1,16 @@
 from gooey.python_bindings import modules
 
-__author__ = 'Chris'
-
-import unittest
-
-
-class TestModules(unittest.TestCase):
-
-  def test_load_creates_and_imports_module_from_string_source(self):
-    module_source = '''
+module_source = \
+'''
 some_var = 1234
 
 def fooey():
   return 10
 
-    '''
+'''
+
+def test_load_creates_and_imports_module_from_string_source():
     module = modules.load(module_source)
-    self.assertEqual(10, module.fooey())
+    assert 10 == module.fooey()
 
 
-  def test_generate_pyfilename_does_not_begin_with_digit(self):
-    self.assertTrue(not modules.generate_pyfilename()[0].isdigit())
