8603c34b62b6d79bd623afa67289c3416817c94d
Roshgar <rcourture@gmail.com>
2015-06-04 02:46:17 +0200

    Update widget_pack.py

--- gooey/gui/widgets/widget_pack.py
+++ gooey/gui/widgets/widget_pack.py
@@ -7,6 +7,7 @@ __author__ = 'Chris'
 from abc import ABCMeta, abstractmethod
 
 import wx
+import os
 import wx.lib.agw.multidirdialog as MDD
 
 from gooey.gui.widgets.calender_dialog import CalendarDlg
