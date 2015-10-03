17d1d67913a4a2bcbc263327b8a4f1796750ba5b
chriskiehl <audionautic@gmail.com>
2015-10-03 15:15:37 -0400

    Converted html to ReST because PyPi is dumb

--- setup.py
+++ setup.py
@@ -30,31 +30,32 @@ setup(
         'Programming Language :: Python :: 2.7'
     ],
     long_description='''
-<h1>Gooey (Beta)</h1>
 
-<h3>Turn (almost) any Python Console Program into a GUI application with one line</h3>
-
-<p align="center">
-    <img src="https://cloud.githubusercontent.com/assets/1408720/7904381/f54f97f6-07c5-11e5-9bcb-c3c102920769.png"/>
-</p>
+Gooey (Beta)
+############
 
 
-<h2>Quick Start</h2>
+Turn (almost) any Python Console Program into a GUI application with one line
+-----------------------------------------------------------------------------
 
-<p>Gooey is attached to your code via a simple decorator on your `main` method.</p>
+.. image:: https://cloud.githubusercontent.com/assets/1408720/7904381/f54f97f6-07c5-11e5-9bcb-c3c102920769.png
 
-<pre>
-    from gooey import Gooey
 
-    @Gooey      <--- all it takes! :)
-    def main():
+Quick Start
+-----------
+
+Gooey is attached to your code via a simple decorator on your `main` method.
+
+.. code-block::
+
+  from gooey import Gooey
+  @Gooey      <--- all it takes! :)
+  def main():
       # rest of code
 
-</pre>
+
 
 With the decorator attached, run your program and the GUI will now appear!
 
-<b>Note: PyPi's formatting is ancient, so checkout the full documentation, instructions, and source on <a href="https://github.com/chriskiehl/Gooey">github!</a></b>
-
-<br /><br /><br /><br />'''
+Note: PyPi's formatting is ancient, so checkout the full documentation, instructions, and source on `Github <https://github.com/chriskiehl/Gooey>`_'''
 )
