ee1ed8abacaeba24709e82a8c2e89e07953f6040
Alexander Gordeyev <s0meuser@yandex.ru>
2015-10-23 18:37:01 +0300

    add progress bar examples

---
+++ gooey/_tmp/example_progress_bar_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import print_function
+import sys
+from time import sleep
+from gooey import Gooey, GooeyParser
+
+
+@Gooey(progress_regex=r"^progress: (\d+)%$")
+def main():
+    parser = GooeyParser(prog="example_progress_bar_1")
+    _ = parser.parse_args(sys.argv[1:])
+
+    for i in range(100):
+        print("progress: {}%".format(i+1))
+        sys.stdout.flush()
+        sleep(0.1)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ gooey/_tmp/example_progress_bar_3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import print_function
+import sys
+from time import sleep
+from gooey import Gooey, GooeyParser
+
+
+@Gooey(progress_regex=r"^progress: (?P<current>\d+)/(?P<total>\d+)$",
+       progress_expr="current / total * 100")
+def main():
+    parser = GooeyParser(prog="example_progress_bar_3")
+    parser.add_argument("steps", type=int, default=15)
+    parser.add_argument("delay", type=int, default=1)
+    args = parser.parse_args(sys.argv[1:])
+
+    for i in range(args.steps):
+        print("progress: {}/{}".format(i+1, args.steps))
+        sys.stdout.flush()
+        sleep(args.delay)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
