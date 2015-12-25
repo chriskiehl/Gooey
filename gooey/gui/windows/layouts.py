cfdfda37a35c007565db2108499a659d4d9e3307
chriskiehl <audionautic@gmail.com>
2015-12-25 10:14:31 -0500

    Fix build.conf typo; fix basic_layout json structure

--- gooey/gui/windows/layouts.py
+++ gooey/gui/windows/layouts.py
@@ -10,17 +10,18 @@ from gooey.gui.util.quoting import quote
 
 
 basic_config = {
-    'required': [{
+    'widgets': [{
       'type': 'CommandField',
+      'required': True,
       'data': {
         'display_name': 'Enter Commands',
         'help': 'Enter command line arguments',
         'nargs': '',
         'commands': '',
         'choices': [],
+        'default': None,
       }
     }],
-    'optional': []
 }
 
 
