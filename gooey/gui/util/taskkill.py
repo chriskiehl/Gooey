2145ee5961cc35d36013e2333c636c7390b6c039
Jonathan Schultz <jonathan@schultz.la>
2017-07-17 14:08:58 +1000

    Kill child processes as well as shell process

--- gooey/gui/util/taskkill.py
+++ gooey/gui/util/taskkill.py
@@ -7,5 +7,9 @@ if sys.platform.startswith("win"):
   def taskkill(pid):
     os.system('taskkill /F /PID {:d} /T >NUL 2>NUL'.format(pid))
 else:  # POSIX
+  import psutil
   def taskkill(pid):
-    os.kill(pid, signal.SIGTERM)
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+      child.kill()
+    parent.kill()
