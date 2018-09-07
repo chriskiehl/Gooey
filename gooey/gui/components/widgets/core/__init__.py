aa0add68ea7f414c4bda2f0b9caeade099c3f52e
HenrykHaniewicz <henryk.haniewicz@gmail.com>
2018-09-07 19:09:15 +0200

    Added MultiDirChooser to class list

--- gooey/gui/components/widgets/core/__init__.py
+++ gooey/gui/components/widgets/core/__init__.py
@@ -1,2 +1,2 @@
-from . chooser import Chooser, FileChooser, FileSaver, DirChooser, DateChooser, MultiFileChooser
+from . chooser import Chooser, FileChooser, FileSaver, DirChooser, DateChooser, MultiFileChooser, MultiDirChooser
 from . text_input import PasswordInput, MultilineTextInput, TextInput
