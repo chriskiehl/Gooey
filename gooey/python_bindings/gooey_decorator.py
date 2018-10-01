854a751439ebd28482160a603a39888206e5f930
Nathan Richard <nathan.richard@etu.utc.fr>
2018-10-01 16:50:51 +0200

    Avoid importing wx when --ignore-gooey is part of the command line arguments.

--- gooey/python_bindings/gooey_decorator.py
+++ gooey/python_bindings/gooey_decorator.py
@@ -11,7 +11,6 @@ import os
 import sys
 from argparse import ArgumentParser
 
-from gooey.gui import application
 from gooey.gui.util.freeze import getResourcePath
 from gooey.util.functional import merge
 from . import config_generator
@@ -53,6 +52,7 @@ def Gooey(f=None,
 
   def build(payload):
     def run_gooey(self, args=None, namespace=None):
+      from gooey.gui import application
       source_path = sys.argv[0]
 
       build_spec = None
