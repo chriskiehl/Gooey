1b46314dfb0ab431ea2d293ea6fe1762da6f28d6
Taysser <tagherfa@microsoft.com>
2019-09-30 13:41:24 -0700

    commit

--- gooey/util/functional.py
+++ gooey/util/functional.py
@@ -46,7 +46,7 @@ def flatmap(f, coll):
 
 
 def indexunique(f, coll):
-    """0123 Build a map from the collection keyed off of f
+    """0000Build a map from the collection keyed off of f
     e.g.
         [{id:1,..}, {id:2, ...}] => {1: {id:1,...}, 2: {id:2,...}}
 
