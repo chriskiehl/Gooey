9fb3a75639b4a979e7441af23beba7dec9add6a7
tpr <admin@axiotonomous.com>
2020-02-10 17:02:28 -0600

    renamed to appropriate name and removed cruft

--- gooey/tests/test_print.py
+++ gooey/tests/test_print.py
@@ -17,10 +17,9 @@ from argparse import ArgumentParser
 )
 def main():
     desc = "Example application to show Gooey's various widgets"
-    parser = GooeyParser(prog="example_progress_bar_1")
+    parser = GooeyParser(prog="example_print")
     _ = parser.parse_args(sys.argv[1:])
 
-    import time
     print('Hello, world!\nNewlines work fine!')
 
 if __name__ == '__main__':
