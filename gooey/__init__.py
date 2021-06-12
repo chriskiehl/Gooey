f771dc66d5173b3800b195d7abf6d9ad9fc57d1a
Chris <audionautic@gmail.com>
2021-06-12 11:50:46 -0700

    bump version and add release notes

--- gooey/__init__.py
+++ gooey/__init__.py
@@ -5,4 +5,4 @@ from gooey.gui.util.freeze import localResourcePath as local_resource_path
 from gooey.python_bindings import constants
 from gooey.gui.components.filtering.prefix_filter import PrefixTokenizers
 from gooey.gui.components.options import options
-__version__ = '1.0.8'
+__version__ = '1.0.8.1'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme:
     long_description = readme.read()
 
-version = '1.0.8'
+version = '1.0.8.1'
 
 deps = [
     'Pillow>=4.3.0',
